7cee0fedf0bfef1a66fdc6697788a3b469828c26
Abimael <abimaelr.albu@gmail.com>
2022-01-06 20:33:48 -0300

    SimpleReport Generate

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+import statistics
+
+
+class SimpleReport:
+    def generate(data):
+        today = datetime.today().strftime("%Y-%m-%d")
+        dates = [date["data_de_fabricacao"] for date in data]
+        validity = [
+            date["data_de_validade"]
+            for date in data
+            if date["data_de_validade"] >= today
+        ]
+        company = [date["nome_da_empresa"] for date in data]
+        return (
+            "Data de fabricação mais antiga: "
+            + min(dates)
+            + "\nData de validade mais próxima: "
+            + min(validity)
+            + "\nEmpresa com maior quantidade de produtos estocados: "
+            + statistics.mode(company)
+        )
