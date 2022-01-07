8f59a66e602f77311b5521c36cd77049f1092bf3
Abimael <abimaelr.albu@gmail.com>
2022-01-06 21:55:54 -0300

    Complete report

---
+++ inventory_report/reports/complete_report.py
@@ -0,0 +1,30 @@
+from inventory_report.reports.simple_report import SimpleReport
+from datetime import datetime
+import statistics
+
+
+class CompleteReport(SimpleReport):
+    def generate(data):
+        today = datetime.today().strftime("%Y-%m-%d")
+        dates = [date["data_de_fabricacao"] for date in data]
+        validity = [
+            date["data_de_validade"]
+            for date in data
+            if date["data_de_validade"] >= today
+        ]
+        companies = [date["nome_da_empresa"] for date in data]
+        stock = ""
+        for company in set(companies):
+            stock += (
+                "- " + company + ": " + str(companies.count(company)) + "\n"
+            )
+        return (
+            "Data de fabricação mais antiga: "
+            + min(dates)
+            + "\nData de validade mais próxima: "
+            + min(validity)
+            + "\nEmpresa com maior quantidade de produtos estocados: "
+            + statistics.mode(companies)
+            + "\n\nProdutos estocados por empresa:\n"
+            + stock
+        )
