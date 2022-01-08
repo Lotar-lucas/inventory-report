820832e400131e3ea5758e09bb26bafd30cddf7b
Abimael <abimaelr.albu@gmail.com>
2022-01-08 20:45:29 -0300

    inventory

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,11 +1,15 @@
 from ..reports import simple_report, complete_report
+import csv
 
 
 class Inventory():
 
     def import_data(path, type):
+        with open(path, mode='r') as csv_file:
+            data = list(csv.DictReader(csv_file, delimiter=','))
+
         switch = {
             "simple_report": simple_report,
             "complete_report": complete_report,
         }
-        return switch[type](path)
+        return switch[type](data)
