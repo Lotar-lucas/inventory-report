3b0170e23d13661b180a8bb86350a7a27d6c29a6
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2022-01-07 21:38:06 -0300

    fix, delete comment

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,5 +1,4 @@
 from inventory_report.reports.simple_report import SimpleReport
-from datetime import datetime
 
 
 class CompleteReport(SimpleReport):
@@ -14,28 +13,6 @@ class CompleteReport(SimpleReport):
             )
 
         return f"{simple_report}\n\nProdutos estocados por empresa: \n{stock}"
-
-
-# teste = [
-#     {
-#         "id": 1,
-#         "nome_do_produto": "CALENDULA OFFICINALIS FLOWERING TOP, GERANIUM MACULATUM ROOT, SODIUM CHLORIDE, THUJA OCCIDENTALIS LEAFY TWIG, ZINC, and ECHINACEA ANGUSTIFOLIA",
-#         "nome_da_empresa": "Forces of Nature",
-#         "data_de_fabricacao": "2020-07-04",
-#         "data_de_validade": "2023-02-09",
-#         "numero_de_serie": "FR48 2002 7680 97V4 W6FO LEBT 081",
-#         "instrucoes_de_armazenamento": "in blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices phasellus",
-#     },
-#     {
-#         "id": 2,
-#         "nome_do_produto": "CALENDULA OFFICINALIS FLOWERING TOP, GERANIUM MACULATUM ROOT, SODIUM CHLORIDE, THUJA OCCIDENTALIS LEAFY TWIG, ZINC, and ECHINACEA ANGUSTIFOLIA",
-#         "nome_da_empresa": "Garoto",
-#         "data_de_fabricacao": "2020-07-04",
-#         "data_de_validade": "2023-02-09",
-#         "numero_de_serie": "FR48 2002 7680 97V4 W6FO LEBT 081",
-#         "instrucoes_de_armazenamento": "in blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices phasellus",
-#     },
-# ]
 
 
 # print(CompleteReport.generate(teste))
