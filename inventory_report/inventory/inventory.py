bea131cd25268f4b38c943dbf078366fde0ed7bd
Abimael <abimaelr.albu@gmail.com>
2022-01-08 21:48:46 -0300

    test

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,13 +1,27 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
+    @staticmethod
+    def readType(path):
+        file_type = path.split('.')[1]
+        out = []
+
+        if(file_type == 'csv'):
+            with open(path, mode='r') as csv_file:
+                out = list(csv.DictReader(csv_file, delimiter=','))
+            return out
+        elif(file_type == 'json'):
+            with open(path) as file:
+                jsonFile = file.read()
+                out = json.loads(jsonFile)
+            return out
 
     def import_data(path, type):
-        with open(path, mode='r') as csv_file:
-            data = list(csv.DictReader(csv_file, delimiter=','))
+        data = Inventory.readType(path)
         switch = {
             "simples": SimpleReport,
             "completo": CompleteReport,
