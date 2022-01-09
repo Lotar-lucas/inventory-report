d51dbbd36e620f85b94deb9c4689d0c1fb9f9a48
Abimael <abimaelr.albu@gmail.com>
2022-01-09 10:06:16 -0300

    Importer Class

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,36 +1,19 @@
+from inventory_report.importer.importer import Importer
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
-import csv
-import json
-import xml.etree.ElementTree as xml
 
 
-class Inventory:
+class Inventory(Importer):
     @staticmethod
     def readType(path):
         file_type = path.split(".")[1]
-        out = []
 
         if file_type == "csv":
-            with open(path, mode="r") as csv_file:
-                out = list(csv.DictReader(csv_file, delimiter=","))
-            return out
+            return Importer.CsvImporter(path)
         elif file_type == "json":
-            with open(path) as file:
-                jsonFile = file.read()
-                out = json.loads(jsonFile)
-            return out
+            return Importer.JsonImporter(path)
         else:
-            # I got some help from: https://www.py4u.net/discuss/18657
-            file = xml.parse(path)
-            keys = file.getroot()
-            out = []
-            for key in keys:
-                temp = {}
-                for attribute in key:
-                    temp[attribute.tag] = attribute.text
-                out.append(temp)
-            return out
+            return Importer.XmlImporter(path)
 
     def import_data(path, type):
         data = Inventory.readType(path)
